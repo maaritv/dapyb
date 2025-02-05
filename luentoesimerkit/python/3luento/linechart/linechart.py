36e3b9ee7f14e3fd49f177b1b6deb731d93fb114
Maarit Välisuo <mvalisuo@MacBookAir.lan>
2025-02-05 20:03:02 +0200

    Koodi refaktoroitu.

--- luentoesimerkit/python/3luento/linechart/linechart.py
+++ luentoesimerkit/python/3luento/linechart/linechart.py
@@ -1,27 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Data: years and sales trends
+
+# Create the line chart
+def create_line_chart(title, xlabel, ylabel):
+    plt.figure(figsize=(10, 6))
+    # Add labels, title, and legend
+    plt.title(title, fontsize=14)
+    plt.xlabel(xlabel, fontsize=12)
+    plt.ylabel(ylabel, fontsize=12)
+    plt.grid(True)
+    #plt.tight_layout()
+    return plt
+
+def add_line_to_chart(plot, x, y, label, color):
+    plot.plot(x, y, marker='o', label=label, color=color, linewidth=2)
+    plot.savefig("sales.png") 
+    plot.savefig("sales.pdf")
+    plot.savefig("sales.svg")
+
+title='Omenien ja päärynöiden myynnin kehitys (2017-2024)'
+apple_label='Omenat (euroa)'
+pear_label='Päärynät (euroa)'
+apple_color='red'
+pear_color='blue'
+xlabel="Vuosi"
+ylabel="Myynti (euroa)"
+
 years = np.arange(2017, 2024)
 apple_sales = [2000, 2100, 1900, 2600, 2800, 2700, 3000]  # Npuseva trendi nähtävissä
 pear_sales = [4000, 4100, 3800, 3100, 3000, 2500, 2400]   # Laskevaa trendiä
 
-# Create the line chart
-plt.figure(figsize=(10, 6))
-plt.plot(years, apple_sales, marker='o', label='Omenat (euroa)', color='red', linewidth=2)
-plt.plot(years, pear_sales, marker='o', label='Päärynät (euroa)', color='green', linewidth=2)
+create_line_chart(title, xlabel, ylabel)
+add_line_to_chart(plt, years, pear_sales, pear_label, pear_color)
+add_line_to_chart(plt, years, apple_sales, apple_label, apple_color)
 
-# Add labels, title, and legend
-plt.title('Omenien ja päärynöiden myynnin kehitys (2017-2024)', fontsize=14)
-plt.xlabel('Vuosi', fontsize=12)
-plt.ylabel('Myynti (euroa)', fontsize=12)
-plt.legend(fontsize=12)
-plt.grid(True)
-plt.tight_layout()
-
-##talletetaan kuva tiedostoon raporttiin liittämistä varten
-plt.savefig("sales.png") 
-plt.savefig("sales.pdf")
-plt.savefig("sales.svg")
 
 
